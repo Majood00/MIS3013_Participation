924c1b75f4a6a549118f7d1e51705e9d91e47a04
Majood00 <majood00@ou.edu>
2021-07-13 15:11:43 -0500

    Solution for SumOf3 participation.

--- SumOf3/SumOf3/Program.cs
+++ SumOf3/SumOf3/Program.cs
@@ -18,10 +18,12 @@ namespace SumOf3
             Console.WriteLine($"The numbers you input are {number_1}, {number_2} and {number_3}.");
 
             double calculation_1 = number_1 + number_2 + number_3;
-            string calculation1 = calculation_1.ToString("D3");
+            string calculation1 = calculation_1.ToString("N3");
+            Console.WriteLine(calculation1);
             const double numberValue = 7.777;
             double calculation_2 = calculation_1 * numberValue;
-            string calculation2 = calculation_2.ToString("D3");
+            string calculation2 = calculation_2.ToString("N3");
+            Console.WriteLine(calculation2);
         }
     }
 }

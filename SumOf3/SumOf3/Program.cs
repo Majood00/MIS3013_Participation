18bd8f2f6532a75221cf66d79a97bfbf7845d9dd
Majood00 <majood00@ou.edu>
2021-07-13 15:06:28 -0500

    SumOf3 partial answer

---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SumOf3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter a number. Any number is acceptable incuding decimals.");
+            double number_1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter a new number.");
+            double number_2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Please enter a third number.");
+            double number_3 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The numbers you input are {number_1}, {number_2} and {number_3}.");
+
+            double calculation_1 = number_1 + number_2 + number_3;
+            string calculation1 = calculation_1.ToString("D3");
+            const double numberValue = 7.777;
+            double calculation_2 = calculation_1 * numberValue;
+            string calculation2 = calculation_2.ToString("D3");
+        }
+    }
+}

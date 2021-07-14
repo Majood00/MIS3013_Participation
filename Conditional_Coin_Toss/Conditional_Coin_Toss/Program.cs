013f2f1ee8d45a2dd14e751aebb4afc625fe27ed
Majood00 <majood00@ou.edu>
2021-07-14 14:54:01 -0500

    solution for coin toss participation

---
+++ Conditional_Coin_Toss/Conditional_Coin_Toss/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Conditional_Coin_Toss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 3);
+            Console.WriteLine(number);
+
+            Console.WriteLine("Heads or Tails? Please select one.");
+            string response = Console.ReadLine();
+
+            string guess;
+
+            if (number == 1)
+            {
+                guess = "Heads";
+            }
+            else
+            {
+                guess = "Tails";
+            }
+
+            if (response.ToUpper() == guess.ToUpper())
+            {
+                Console.WriteLine("Your guess was correct.");
+            }
+            else
+            {
+                Console.WriteLine("Your guess was wrong!");
+            }
+
+            const string name = "Majood Haddad";
+            Console.WriteLine(name);
+        }
+    }
+}

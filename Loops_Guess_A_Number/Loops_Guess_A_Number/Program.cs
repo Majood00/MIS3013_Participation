48950f4e53cfd8611dddd06d5989a2818a76e461
Majood00 <majood00@ou.edu>
2021-07-21 13:34:46 -0500

    added some  colors to the guess a number participation

--- Loops_Guess_A_Number/Loops_Guess_A_Number/Program.cs
+++ Loops_Guess_A_Number/Loops_Guess_A_Number/Program.cs
@@ -17,6 +17,7 @@ namespace Loops_Guess_A_Number
 
             Random rand = new Random();
             int number = rand.Next(lower, upper + 1);
+            Console.WriteLine(number);
 
             int guessedNumber;
 
@@ -32,19 +33,26 @@ namespace Loops_Guess_A_Number
                 }
                 else if (guessedNumber < number)
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("You guessed too low.");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                 else if (guessedNumber > number)
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
                     Console.WriteLine("You guessed too high.");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                else
                 {
+                    
                 }
 
             } while (guessedNumber != number);
 
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine($"You guessed it! The correct number is {number}.");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Exiting the appliation.");
             Environment.Exit(0);
             

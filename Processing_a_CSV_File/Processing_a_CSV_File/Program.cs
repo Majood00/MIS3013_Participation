2bc87e6d7429b9b763bd00ba5086e29c713cc3e3
Majood00 <majood00@ou.edu>
2021-07-24 13:15:29 -0500

    completed processing a csv file participation

--- Processing_a_CSV_File/Processing_a_CSV_File/Program.cs
+++ Processing_a_CSV_File/Processing_a_CSV_File/Program.cs
@@ -15,23 +15,32 @@ namespace Processing_a_CSV_File
             double salesFor2004 = 0;
             double salesFor2005 = 0;
 
+            string months = "January,February,March,April,May,June,July,August,September,October,November,December";
 
+            string[] seperatedMonths = months.Split(',');
+
+            double[] monthlySalesAcrossAllYears = new double[12];
 
             for (int i = 1; i < contents.Length; i++)
             {
 
-                string line = contents[i];
+                    string line = contents[i];
 
                 string[] pieces = line.Split(',');
 
                 double sale = Convert.ToDouble(pieces[4]);
 
+                double saleForMonth = Convert.ToDouble(pieces[4]);
+
                 int year = Convert.ToInt32(pieces[9]);
 
                 string status = pieces[6];
 
+                int month = Convert.ToInt32(pieces[8]);
+
                 if (status == "Shipped")
                 {
+                    monthlySalesAcrossAllYears[month - 1] += saleForMonth;
                     if (year == 2003)
                     {
                         salesFor2003 += sale;
@@ -48,15 +57,19 @@ namespace Processing_a_CSV_File
                     {
                         Console.WriteLine($"ERROR: No year for {year}");
                     }
+
                 }
 
-                // Console.WriteLine(sale);
             }
 
             Console.WriteLine($"The sales for 2003 were {salesFor2003.ToString("C")}");
             Console.WriteLine($"The sales for 2004 were {salesFor2004.ToString("C")}");
             Console.WriteLine($"The sales for 2005 were {salesFor2005.ToString("C")}");
 
+            for (int i = 0; i < 12; i++)
+            {
+                Console.WriteLine($"The sales for {seperatedMonths[i]} were {monthlySalesAcrossAllYears[i].ToString("C")}");
+            }
 
         }
     }

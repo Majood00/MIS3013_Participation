42dadb71acfd2675ffb6cc6a1967db3ce14bc27e
Majood00 <majood00@ou.edu>
2021-07-27 13:59:41 -0500

    commit

--- All_types_of_collections!/All_types_of_collections!/Program.cs
+++ All_types_of_collections!/All_types_of_collections!/Program.cs
@@ -71,46 +71,60 @@ namespace All_types_of_collections_
 
             average = sum / coursegrades.Count;
             Console.WriteLine($"Your grade average is {average.ToString("N2")}");
-
+            
             // Dictionary
             Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
 
-            List<double> list1 = new List<double>();
-            list1.Add(0.95);
-            list1.Add(0.80);
-            list1.Add(0.75);
+            students.Add("MIS3013", new List<double>());
+            students["MIS3013"].Add(0.95);
+            students["MIS3013"].Add(0.5);
+            students["MIS3013"].Add(0.7);
 
-            List<double> list2 = new List<double>();
-            list2.Add(0.85);
-            list2.Add(0.70);
-            list2.Add(0.85);
+            students.Add("MTHR1113", new List<double>());
+            students["MTHR1113"].Add(.6);
+            students["MTHR1113"].Add(0.4);
+            students["MTHR1113"].Add(1.0);
 
-            List<double> list3 = new List<double>();
-            list3.Add(1.00);
-            list3.Add(0.90);
-            list3.Add(0.95);
+            students.Add("BME3333", new List<double>());
+            students["BME3333"].Add(0.75);
+            students["BME3333"].Add(0.85);
+            students["BME3333"].Add(0.95);
 
-            students.Add("BME 3333", list1);
-            students.Add("MIS 3013", list2);
-            students.Add("MTHR 1113", list3);
+            //Extra portion
+            do
+            {
+                Console.WriteLine("Please enter the course code");
+                string coursecode = Console.ReadLine().ToUpper();
 
-            List<List<double>> studentGrades = new List<List<double>>();
-            studentGrades.Add(list1);
-            studentGrades.Add(list2);
-            studentGrades.Add(list3);
+                if (students.ContainsKey(coursecode) == false)
+                {
+                    students.Add(coursecode, new List<double>());
+                }
 
-            double total = 0;
+                do
+                {
+                    Console.WriteLine($"Enter the students grade for {coursecode}");
+                    double grade = Convert.ToDouble(Console.ReadLine());
+                    students[coursecode].Add(grade);
+
+                    Console.WriteLine("Do you have another grade to enter? yes or no");
+
+                } while (Console.ReadLine().ToLower() == "yes");
+
+                Console.WriteLine("Do you have another course to enter grades for? yes or no");
+
+            } while (Console.ReadLine().ToLower() == "yes");
             
             foreach (string course in students.Keys)
             {
-                total = 0;
+                double total = 0;
 
-                foreach (var grade in students[course])
+                foreach (double grade in students[course])
                 {
                    total += grade;
                 }
 
-                double aver = total / students.Values.Count;
+                double aver = total / students[course].Count;
                 Console.WriteLine($"Your grade average for {course} is {aver.ToString("P")}");
             }
 

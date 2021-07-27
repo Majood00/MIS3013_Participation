395238e6f4c8ce8c4244ba25860c991030e48ee6
Majood00 <majood00@ou.edu>
2021-07-27 14:27:20 -0500

    did some edits

--- Collections_Min_Max_Avg/Collections_Min_Max_Avg/Program.cs
+++ Collections_Min_Max_Avg/Collections_Min_Max_Avg/Program.cs
@@ -8,29 +8,30 @@ namespace Collections_Min_Max_Avg
     {
         static void Main(string[] args)
         {
-            string input;
-
             List<double> grades = new List<double>();
 
-            double grade;
+            Dictionary<double, int> repetitions = new Dictionary<double, int>();
+
 
             do
             {
                 Console.WriteLine("Please enter your exam grade.");
-                input = Console.ReadLine();
+                double input = Convert.ToDouble(Console.ReadLine());
 
-                while (double.TryParse(input, out grade) == false)
+                grades.Add(input);
+
+                if (repetitions.ContainsKey(input) == false)
                 {
-                    Console.WriteLine($"{input} is not a valid grade. Please enter a valid grade");
-                    input = Console.ReadLine();
+                    repetitions.Add(input, 1);
+                }
+                else
+                {
+                    repetitions[input]++;
                 }
 
-                grades.Add(grade);
-
                 Console.WriteLine("Do you want to enter another number? yes or no?");
-                input = Console.ReadLine();
 
-            } while (input.ToLower()[0] == 'y');
+            } while (Console.ReadLine() == "yes");
 
             double min = grades[0];
 
@@ -41,6 +42,8 @@ namespace Collections_Min_Max_Avg
                     min = value;
                 }
             }
+
+            int mode = 1;
 
             double max = grades[0];
             foreach (double value in grades)
@@ -64,22 +67,24 @@ namespace Collections_Min_Max_Avg
             Console.WriteLine($"Your maximum exam score is {max.ToString("N2")}");
             Console.WriteLine($"Your average exam score is {ave.ToString("N2")}");
 
-            Dictionary<double, int> repetitions = new Dictionary<double, int>();
-            foreach (var val in grades)
+
+            foreach(double grade in repetitions.Keys)
             {
-                if (repetitions.ContainsKey(val))
+                sum = sum + repetitions[grade] * grade;
+                if (mode < repetitions[grade])
                 {
-                    repetitions[val]++;
-                } 
-                else
-                {
-                    repetitions.Add(val, 1);
-                }        
+                    mode = repetitions[grade];
+                }
             }
 
-            var mode = repetitions.OrderByDescending(x => x.Value).First();
-
-            Console.WriteLine("The mode for your grades is {0}.", mode.Key);
+            foreach (double grade in repetitions.Keys) //Output any keys(grades) that have a value of the mode
+            {
+                sum = sum + repetitions[grade] * grade;
+                if (mode == repetitions[grade])
+                {
+                    Console.WriteLine($"Your mode: \n[{grade}]: " + mode);
+                }
+            }
 
         }
     }

aca170dfb9972dd0c0840f2a9746a9deeda82fb7
Majood00 <majood00@ou.edu>
2021-07-27 13:09:04 -0500

    commit

--- Simple_Method_(Speak)/Simple_Method_(Speak)/Program.cs
+++ Simple_Method_(Speak)/Simple_Method_(Speak)/Program.cs
@@ -6,9 +6,38 @@ namespace Simple_Method__Speak_
     {
         static void Main(string[] args)
         {
+            string sounds = "Please enter a type of animal";
             
-
+            string sound = Speak(sounds);
+            
+            Console.WriteLine($"A ... makes a {sound} sound.");
 
         }
+
+        static string Speak(string sounds)
+        {
+            Console.WriteLine(sounds);
+            string animal = Console.ReadLine().ToLower();
+
+            if (animal == "wolf")
+            {
+                sounds = "Woof";
+            }
+            else if (animal == "cat")
+            {
+                sounds = "Meow";
+            }
+            else if (animal == "snake")
+            {
+                sounds = "Sssssssss";
+            }
+            else
+            {
+                sounds = "Buzzz";
+            }
+            
+            return sounds;
+        }
+
     }
 }

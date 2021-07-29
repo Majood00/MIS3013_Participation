75499e2ed8f88ab93104dd2f60220d056a714af0
Majood00 <majood00@ou.edu>
2021-07-29 14:49:57 -0500

    finished the Classes-Toy Participation

---
+++ Classes-Toy/Classes-Toy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Classes_Toy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Toy toy1 = new Toy();
+            toy1.Name = "Train";
+            toy1.Price = 7.50;
+            toy1.Manufacturer = "Toyfinity";
+            toy1.Note("Hope you will enjoy your time playing with this toy");
+
+            Toy toy2 = new Toy();
+            toy2.Name = "Jigsaw Puzzle";
+            toy2.Price = 12.00;
+            toy2.Manufacturer = "Artifact Puzzles";
+            toy2.Note("Have fun and work hard");
+
+            List<Toy> toys = new List<Toy>();
+            toys.Add(toy1);
+            toys.Add(toy2);
+
+            Console.WriteLine(toy1);
+            Console.WriteLine();
+            Console.WriteLine(toy2);            
+        }
+    }
+}
---
+++ Classes-Toy/Classes-Toy/Toy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_Toy
+{
+    public class Toy
+    {
+        public string Manufacturer { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        private string Notes;
+
+        public Toy()
+        {
+            Manufacturer = string.Empty;
+
+            Name = string.Empty; ;
+
+            Price = 0;
+
+            Notes = string.Empty;
+        }
+
+        public void Note(string comment)
+        {
+            Notes = comment;
+        }
+
+        public string GetAisle()
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 25);
+
+            string aisle = $"{Manufacturer.ToUpper()[0]}{number}";
+            return aisle;
+        }
+
+        public override string ToString()
+        {
+            string aisle = GetAisle();
+            string output = $"{Name} is manufactured by {Manufacturer} and costs {Price.ToString("C")}. It can be found in aisle {aisle}. \nThis toy comes with a nice note: \n{Notes}.";
+
+            return output;
+        }
+}
+}
